ce7ea3ed2b74a6afe36dab2c9e4475a41e4d178b
chanho0912 <chanho0912@naver.com>
2021-09-30 22:15:15 +0900

    data add

---
+++ src/main/java/com/_chanho/movie_recommendation/movie/Movies.java
@@ -0,0 +1,24 @@
+package com._chanho.movie_recommendation.movie;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Movies {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
